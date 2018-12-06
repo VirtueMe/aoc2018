0413e3f601d8fc92c5484ea5ceff2b39d5506702
Benny Thomas <jan.thomas@speilbildet.com>
2018-12-06 09:42:53 +0100

    refactor parse/best-parse day 05

--- src/aoc/day05.clj
+++ src/aoc/day05.clj
@@ -11,30 +11,42 @@
   (if hit
     (if (empty? parsed)
       [(drop 2 list) parsed]
-      [(concat [(last parsed)] (drop 2 list)) (butlast parsed)])
-    [(drop 1 list) (concat parsed [one])]))
+      [(concat [(peek parsed)] (drop 2 list)) (vec (butlast parsed))])
+    [(drop 1 list) (conj parsed one)]))
   
 (defn hit?
   ""
   [one two]
-  (if (Character/isLowerCase one)
-    (and (Character/isUpperCase two) (= one (Character/toLowerCase two)))
-    (and (Character/isLowerCase two) (= one (Character/toUpperCase two)))))
+  (= 32 (Math/abs (- (int one) (int two)))))
 
 ;; Part 1
 (defn parse
   ""
   [input]
-  (loop [list (seq input) parsed '()]
+  (loop [list (seq input) parsed []]
     (if (empty? list)
       parsed
-      (let [[one two] (take 2 list)]
+      (let [[one two] list]
         ;; (println one " : " two " : " list " : " parsed)
         (if (nil? two)
           (concat parsed [one])
           (let [hit (hit? one two)]
-            (let [params (calc-params hit one two list parsed)]
-              (recur (first params) (second params)))))))))
+            (let [[newlist newparsed] (calc-params hit one two list parsed)]
+              (recur newlist newparsed))))))))
+
+;; Part 1 second solution
+;; I didn't like the speed, nor the complexity of parse, so I rewrote it without using a buffer.
+(defn parse2
+  ""
+  [input]
+  (loop [list (if (string? input) input (apply str input)) pos 0]
+    (if (= (count list) (inc pos))
+      list
+      (let [[a b] (seq (subs list pos (+ pos 2)))]
+        (if (hit? a b)
+          (recur (str (subs list 0 pos) (subs list (+ pos 2))) (if (= pos 0) pos (dec pos)))
+          (recur list (inc pos)))))))
+          
 
 ;; Part 2
 (defn only-lower
@@ -54,28 +66,30 @@
   [char]
   [(Character/toUpperCase char) char])
 
-(defn sum-pair
-  ""
-  [total list pair]
-  (- total (+ (list (first pair)) (list (second pair)))))
-    
 (defn best-parse
   ""
   [input]
   (let [list (frequencies input)]
-    (let [total (count input) pairs (map create-pair (reduce #(only-lower %1 %2) [] list))]
+    (let [pairs (map create-pair (reduce #(only-lower %1 %2) [] list))]
       (apply min (map count (map parse (map #(strip input %) pairs)))))))
+
+(defn best-parse2
+  ""
+  [input]
+  (let [list (frequencies input)]
+    (let [pairs (map create-pair (reduce #(only-lower %1 %2) [] list))]
+      (apply min (map count (map parse2 (map #(strip input %) pairs)))))))
 
 (defn solve05-1
   ""
   ([]
-    (count (parse input05)))
+    (count (parse2 input05)))
   ([input]
-    (count (parse input))))
+    (count (parse2 input))))
     
 (defn solve05-2
   ""
   ([]
-    (best-parse input05))
+    (best-parse2 input05))
   ([input]
-    (best-parse input)))
+    (best-parse2 input)))
